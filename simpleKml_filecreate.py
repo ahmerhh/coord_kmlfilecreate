f8200ca5bf505463a96892b247fa5794879fed78
Ahmer <ahmerhabibb@gmail.com>
2022-02-04 11:44:14 +0000

    update

--- simpleKml_filecreate.py
+++ simpleKml_filecreate.py
@@ -1,3 +1,5 @@
+from hashlib import new
+from tkinter import E
 import shapefile
 import simplekml
 import os
@@ -9,22 +11,14 @@ filename = input("Input file location: ")
 shape = shapefile.Reader(filename)
 file_save_name = input("Enter file name: ")
 file_des = input("File description: ")
-
-
 #KML
 kml = simplekml.Kml()
 kml.document.name = file_save_name
-
 emptyCoord = []
 for x in range(len(shape)):
     feature = shape.shapeRecords()[x]
     first = feature.shape.__geo_interface__
     coordinates = list(first.values())[1]
     emptyCoord.append(coordinates)
-
-
-    
-
 lin = kml.newlinestring(name=kml.document.name, description=file_des, coords=emptyCoord)
-
-kml.save(os.path.splitext(file_save_name)[0] + ".kml")
+kml.save(file_save_name + ".kml")
