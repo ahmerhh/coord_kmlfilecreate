b93da37a16d8a37d972414f00780973f00562425
Ahmer <ahmerhabibb@gmail.com>
2022-02-09 23:58:53 +0000

    Remove lines

--- simpleKml_filecreate.py
+++ simpleKml_filecreate.py
@@ -14,11 +14,13 @@ file_des = input("File description: ")
 #KML
 kml = simplekml.Kml()
 kml.document.name = file_save_name
+
 emptyCoord = []
 for x in range(len(shape)):
     feature = shape.shapeRecords()[x]
     first = feature.shape.__geo_interface__
     coordinates = list(first.values())[1]
     emptyCoord.append(coordinates)
+
 lin = kml.newlinestring(name=kml.document.name, description=file_des, coords=emptyCoord)
 kml.save(file_save_name + ".kml")
